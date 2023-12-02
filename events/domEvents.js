e7000e11c8882f0399279699b57557bde663db27
Thomas McMahon <glassoctopus@yahoo.com>
2023-12-01 23:36:17 -0600

    a little further, working on view book details.

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,5 +1,6 @@
-import { getBooks, deleteBook } from '../api/bookData';
+import { getBooks, deleteBook, getSingleBook } from '../api/bookData';
 import { showBooks } from '../pages/books';
+import addBookForm from '../components/forms/addBookForm';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -18,17 +19,22 @@ const domEvents = () => {
     // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
       console.warn('ADD BOOK');
+      addBookForm();
     }
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
       console.warn('EDIT BOOK', e.target.id);
       console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
       console.warn('VIEW BOOK', e.target.id);
       console.warn(e.target.id.split('--'));
+      // const [, firebaseKey] = e.target.id.split('--');
+      // getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
 
     // FIXME: ADD CLICK EVENT FOR DELETING AN AUTHOR
--- events/formEvents.js
+++ events/formEvents.js
@@ -1,9 +1,28 @@
+import {
+  createBook, updateBook, getBooks, getSingleBook
+} from '../api/bookData';
+import { showBooks } from '../pages/books';
+import addBookForm from '../components/forms/addBookForm';
+
 const formEvents = () => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
     e.preventDefault();
     // TODO: CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
     if (e.target.id.includes('submit-book')) {
-      console.warn('CLICKED SUBMIT BOOK', e.target.id);
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        author_id: document.querySelector('#author_id').value,
+        sale: document.querySelector('#sale').checked,
+      };
+      createBook(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateBook(patchPayload).then(() => {
+          getBooks().then(showBooks);
+        });
+      });
     }
 
     // TODO: CLICK EVENT FOR EDITING A BOOK
@@ -11,6 +30,7 @@ const formEvents = () => {
       const [, firebaseKey] = e.target.id.split('--');
       console.warn('CLICKED UPDATE BOOK', e.target.id);
       console.warn(firebaseKey);
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
 
     // FIXME: ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
